e91a0654ea526c236c612f799da51c3297aad7a1
Julian Goh <juliangoh5@gmail.com>
2021-07-27 20:03:50 +1000

    27/7/21 - Day 1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,87 @@
+/*
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+console.log('Jonas');
+
+let firstName = 'Bob';
+console.log(firstName);
+let three_years = 3;
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1991;
+console.log(typeof year);*/
+
+//Homework
+const country = 'Australia';
+const continent = 'Oceania';
+let population = 20;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isIsland = true;
+let language;
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
+language = 'English';
+console.log(population);
+population += 1;
+console.log(population);
+
+const finlandPopulation = 6;
+console.log(finlandPopulation > population);
+const averageCountryPopulation = 33;
+console.log(population > averageCountryPopulation);
+
+const description = 'Portugal is in Europe, and its 11 million people speak Portuguese';
+console.log(description);
+
+
+let age = 30;
+age = 31;
+console.log(age);
+
+const birthYear = 1991;
+
+var job = 'programmer';
+job = 'teacher';
+console.log(job);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas,ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 2, 2**3);
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 5; // x = x + 5
+console.log(x); 
+
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+console.log (now - 1991 >  now - 2018);
