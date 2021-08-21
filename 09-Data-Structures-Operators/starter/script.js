acc0bd2621c0fd82a01bb4488e4a54cecbd6d1db
Julian Goh <juliangoh5@gmail.com>
2021-08-21 15:53:20 +1000

    21/8/21 - Deconstructing arrays/objects

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,4 +26,95 @@ const restaurant = {
       close: 24,
     },
   },
+  
+  order: function(starterIndex, mainIndex){
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function({starterIndex = 1, mainIndex = 0, time = '20:00', address}){
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  },
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+}); 
+
+restaurant.orderDelivery({
+  address: 'Via del Sole, 21',
+  starterIndex: 1,
+}); 
+
+
+
+const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories);
+
+const{
+  name: restaurantName, 
+  openingHours: hours, 
+  categories: tags
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+
+//Default values
+const{
+  menu = [],  //Establish as default value
+  starterMenu: starters = [],
+} = restaurant;
+
+console.log(menu, starters);
+
+//Mutating variables
+let a = 111;
+let b = 999;
+const obj ={
+  a: 23,
+  b: 7,
+  c: 14, 
+};
+
+({a, b} = obj);
+console.log(a,b)
+
+
+//Nested objects
+// const {fri:{open,close}} = openingHours;
+
+const{
+  fri:{open: fridayOpen},
+  fri:{close: fridayClose},
+} = openingHours;
+
+/*
+const arr = [2, 3, 4];
+const a = arr[1];
+const b = arr[2];
+const c = arr[3];
+
+const [x, y, z] = arr;
+
+
+let [main, , secondary] = restaurant.categories;
+
+[main, secondary] = [secondary, main];
+
+
+const [starter, mainCourse] = restaurant.order(2, 0);
+
+console.log(starter, mainCourse);
+
+const nested = [2, 4, [5, 6]];
+// const [i, ,j] = nested;
+
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+
+//Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+*/
